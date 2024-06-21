61703b4526f2c61f5a22e849f874ee502036914f
Willis Dyson <willisdyson@gmail.com>
2024-06-21 08:48:24 +0100

    Pushing changes

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,9 @@
+export default function Slug() {
+    return (
+        <main>
+            <h1 style={{ color: 'white', textAlign: 'center' }}>
+                Slug
+            </h1>
+        </main>
+    );
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,9 @@
+export default function ShareMeals() {
+    return (
+        <main>
+            <h1 style={{ color: 'white', textAlign: 'center' }}>
+                Share meals
+            </h1>
+        </main>
+    );
+}
